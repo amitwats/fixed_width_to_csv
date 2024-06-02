c6dbcc45a67c0b89194936be73d6ac7dec03770c
Amit Wats <amitwats@gmail.com>
2024-06-02 14:52:56 +1000

    Cleaned main.py

--- main.py
+++ main.py
@@ -1,23 +1,10 @@
-'''
-python -m venv venv
-venv\Scripts\activate
-deactivate
-
-venv\Scripts\python main.py
-venv\Scripts\python -m pip install --upgrade pip
-venv\Scripts\python -m pip install -r requirements.txt
-
-
-'''
-
-
 from parser.fw_2_csv_parser import FW2CSVParser
 from configs.config import INPUT_FILE_PATH, OUTPUT_FILE_PATH, SPECIFICATIONS_FILE 
 
 if __name__=="__main__":
-    # sp=FW2CSVParser.from_json(SPECIFICATIONS_FILE)
-    # sp.parse_fw_to_csv(INPUT_FILE_PATH,OUTPUT_FILE_PATH)
-    
+    sp=FW2CSVParser.from_json(SPECIFICATIONS_FILE)
+    sp.parse_fw_to_csv(INPUT_FILE_PATH,OUTPUT_FILE_PATH)
+
 
 
 
